36e5e32a37470928baa31ff6795040b0e2772fc4
badmomib <alexcarlnordberg@gmail.com>
2023-09-24 19:11:54 +0200

    first-commit

---
+++ src/main/java/net/n8rd/n8rdsfirstmod/N8rdsFirstModClient.java
@@ -0,0 +1,10 @@
+package net.n8rd.n8rdsfirstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class N8rdsFirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/n8rd/n8rdsfirstmod/N8rdsFirstModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.n8rd.n8rdsfirstmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class N8rdsFirstModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
